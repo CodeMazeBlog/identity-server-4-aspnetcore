8563c034a2ae837b18dcb5fa84a8637587a1bbd8
Marinko <marinkospasojevic@gmail.com>
2020-07-08 11:59:48 +0200

    Updated source code for the IdentityServer4 library version 4

--- End/CompanyEmployees.OAuth/CompanyEmployees.OAuth/Configuration/InMemoryConfig.cs
+++ End/CompanyEmployees.OAuth/CompanyEmployees.OAuth/Configuration/InMemoryConfig.cs
@@ -9,47 +9,56 @@ namespace CompanyEmployees.OAuth.Configuration
     public static class InMemoryConfig
     {
         public static IEnumerable<IdentityResource> GetIdentityResources() =>
-            new List<IdentityResource>
-            {
-                new IdentityResources.OpenId(),
-                new IdentityResources.Profile()
-            };
+          new List<IdentityResource>
+          {
+              new IdentityResources.OpenId(),
+              new IdentityResources.Profile()
+          };
+
+        public static IEnumerable<ApiScope> GetApiScopes() =>
+           new List<ApiScope> { new ApiScope("companyApi", "CompanyEmployee API") };
 
         public static IEnumerable<ApiResource> GetApiResources() =>
-            new List<ApiResource> { new ApiResource("companyApi", "CompanyEmployee API") };
+            new List<ApiResource> 
+            { 
+                new ApiResource("companyApi", "CompanyEmployee API") 
+                { 
+                    Scopes = { "companyApi" } 
+                } 
+            };
 
         public static List<TestUser> GetUsers() =>
-           new List<TestUser>
-           {
-                new TestUser
-                {
-                    SubjectId = "a9ea0f25-b964-409f-bcce-c923266249b4",
-                    Username = "Mick",
-                    Password = "MickPassword",
-                    Claims = new List<Claim>
-                    {
-                        new Claim("given_name", "Mick"),
-                        new Claim("family_name", "Mining")
-                    }
-                },
-                new TestUser
-                {
-                    SubjectId = "c95ddb8c-79ec-488a-a485-fe57a1462340",
-                    Username = "Jane",
-                    Password = "JanePassword",
-                     Claims = new List<Claim>
-                    {
-                        new Claim("given_name", "Jane"),
-                        new Claim("family_name", "Downing")
-                    }
-                }
-           };
+          new List<TestUser>
+          {
+              new TestUser
+              {
+                  SubjectId = "a9ea0f25-b964-409f-bcce-c923266249b4",
+                  Username = "Mick",
+                  Password = "MickPassword",
+                  Claims = new List<Claim>
+                  {
+                      new Claim("given_name", "Mick"),
+                      new Claim("family_name", "Mining")
+                  }
+              },
+              new TestUser
+              {
+                  SubjectId = "c95ddb8c-79ec-488a-a485-fe57a1462340",
+                  Username = "Jane",
+                  Password = "JanePassword",
+                  Claims = new List<Claim>
+                  {
+                      new Claim("given_name", "Jane"),
+                      new Claim("family_name", "Downing")
+                  }
+              }
+          };
 
         public static IEnumerable<Client> GetClients() =>
             new List<Client>
             {
-                new Client
-                {
+               new Client
+               {
                     ClientId = "company-employee",
                     ClientSecrets = new [] { new Secret("codemazesecret".Sha512()) },
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPasswordAndClientCredentials,
